c6de0ed3575a9eef897cbd434d531cbd87bd4801
dougzilla32 <dougstein@gmail.com>
2018-10-07 15:25:47 +0900

    'Cancel' for PromiseKit -- provides the ability to cancel promises and promise chains

--- Sources/MKMapSnapshotter+Promise.swift
+++ Sources/MKMapSnapshotter+Promise.swift
@@ -14,8 +14,49 @@ import PromiseKit
     import PromiseKit
 */
 extension MKMapSnapshotter {
+#if swift(>=4.2)
+    /// Starts generating the snapshot using the options set in this object.
+    public func start() -> Promise<Snapshot> {
+        return Promise<Snapshot>(cancellableTask: MKMapSnapshotterTask(self)) { start(completionHandler: $0.resolve) }
+    }
+#else
     /// Starts generating the snapshot using the options set in this object.
     public func start() -> Promise<MKMapSnapshot> {
-        return Promise { start(completionHandler: $0.resolve) }
+        return Promise<MKMapSnapshot>(cancellableTask: MKMapSnapshotterTask(self)) { start(completionHandler: $0.resolve) }
     }
+#endif
+}
+
+private class MKMapSnapshotterTask: CancellableTask {
+    let snapshotter: MKMapSnapshotter
+    var cancelAttempted = false
+    
+    init(_ snapshotter: MKMapSnapshotter) {
+        self.snapshotter = snapshotter
+    }
+    
+    func cancel() {
+        snapshotter.cancel()
+        cancelAttempted = true
+    }
+    
+    var isCancelled: Bool {
+        return cancelAttempted && !snapshotter.isLoading
+    }
+}
+
+//////////////////////////////////////////////////////////// Cancellable wrapper
+
+extension MKMapSnapshotter {
+#if swift(>=4.2)
+    /// Starts generating the snapshot using the options set in this object.
+    public func cancellableStart() -> CancellablePromise<Snapshot> {
+        return cancellable(start())
+    }
+#else
+    /// Starts generating the snapshot using the options set in this object.
+    public func cancellableStart() -> CancellablePromise<MKMapSnapshot> {
+        return cancellable(start())
+    }
+#endif
 }
